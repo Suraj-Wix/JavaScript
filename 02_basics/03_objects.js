4467e52f6abc7bfb5bb399f8977654e023ef197c
Suraj-Wix <surajsharma87899@gmail.com>
2026-06-16 23:19:28 +0530

    objects

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,19 @@
+// singleton
+// Object.create
+
+
+// object literals
+const JsUser = {
+    name: "Hitesh",
+    "full name": "Hitesh Choudhary",
+    age: 18,
+    location: "Jaipur",
+    emil:"hitesh@google.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Monday","Saturday"]
+}
+
+console.log(JsUsers.email);
+console.log(JsUsers["email"]);
+
+
